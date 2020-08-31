92950298078acaa4bac5cd7be1851c8b803c4e35
Denis Fesenko <dfesek1@gmail.com>
2020-08-31 16:53:16 +0300

    added queue collection

---
+++ TestCollections/src/Book/TestCollections.java
@@ -0,0 +1,66 @@
+package Book;
+
+import java.util.Iterator;
+
+public class TestCollections {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ResizingArrayStack<Item> implements Iterable<Item> {
+
+    private Item[] a = (Item[]) new Object[1];
+    private int N = 0;
+
+    private int size() {
+        return N;
+    }
+
+    private boolean isEmpty() {
+        return N == 0;
+    }
+
+    private void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for (int i = 0; i < N; i++) {
+            temp[i] = a[i];
+            a = temp;
+        }
+    }
+
+    public void push(Item item) {
+        if (N == a.length) resize(2 * a.length);
+            a[N++] = item;
+        }
+
+    public Item pop() {
+        Item item = a[--N];
+        a[N] = null;
+        if (N>0 && N == a.length/4) resize(a.length / 2);
+        return item;
+    }
+
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReversArrayIterator();
+    }
+
+    private class ReversArrayIterator implements Iterator<Item> {
+
+        private int i = N;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return a[i--];
+        }
+    }
+}
+
